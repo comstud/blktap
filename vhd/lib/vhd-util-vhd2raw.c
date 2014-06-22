da22ad64eae2cffbee60d1ebb441d60a5953ac54
Chris Behrens <cbehrens@codestud.com>
2014-06-21 22:36:40 -0700

    Add threads support.
    What it says.

--- vhd/lib/vhd-util-vhd2raw.c
+++ vhd/lib/vhd-util-vhd2raw.c
@@ -26,6 +26,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
+#ifdef BUILD_WITH_THREADS
+#include <pthread.h>
+#endif
 
 #include "libvhd.h"
 
@@ -36,8 +39,21 @@ struct _vhd2raw_options {
     char force;
     char sparse;
     char pwrite;
+#ifdef BUILD_WITH_THREADS
+    char threads;
+    char read_ahead_blocks;
+#endif
     int blk_size;
 };
+
+#ifdef BUILD_WITH_THREADS
+struct _read_buffer_list
+{
+    void *read_buf;
+    size_t read_bytes;
+    struct _read_buffer_list *next;
+};
+#endif
 
 struct _vhd2raw_ctx {
     struct _vhd2raw_options  opts;
@@ -47,11 +63,23 @@ struct _vhd2raw_ctx {
     uint64_t cur_write_pos;
     uint64_t next_write_pos;
     void *compare_buf;
+
+#ifdef BUILD_WITH_THREADS
+    int write_result;
+    char done_reading;
+    pthread_mutex_t thr_lock;
+    pthread_cond_t thr_rcond;
+    pthread_cond_t thr_wcond;
+    struct _read_buffer_list *write_ready_head;
+    struct _read_buffer_list *write_ready_tail;
+    struct _read_buffer_list *read_ready_head;
+    struct _read_buffer_list *read_ready_tail;
+#endif
 };
 
 static void _usage(void)
 {
-	printf("options: [-h] [-b blk_size] [-d] [-f] [-S] [-P] <file.vhd> <output>\n");
+	printf("options: [-h] [-b blk_size] [-d] [-f] [-t] [-r <blocks>] [-S] [-P] <file.vhd> <output>\n");
 }
 
 static int _vhd2raw_ctx_init(struct _vhd2raw_ctx *ctx)
@@ -63,6 +91,10 @@ static int _vhd2raw_ctx_init(struct _vhd2raw_ctx *ctx)
     ctx->opts.force = 0;
     ctx->opts.sparse = 1;
     ctx->opts.pwrite = 1;
+#ifdef BUILD_WITH_THREADS
+    ctx->opts.threads = 0;
+    ctx->opts.read_ahead_blocks = 1;
+#endif
 
     ctx->output_fd = -1;
     ctx->raw_device = 0;
@@ -70,6 +102,19 @@ static int _vhd2raw_ctx_init(struct _vhd2raw_ctx *ctx)
     ctx->cur_write_pos = 0;
     ctx->next_write_pos = 0;
     ctx->compare_buf = NULL;
+
+#ifdef BUILD_WITH_THREADS
+    ctx->write_result = 0;
+    ctx->done_reading = 0;
+    pthread_mutex_init(&(ctx->thr_lock), NULL);
+    pthread_cond_init(&(ctx->thr_rcond), NULL);
+    pthread_cond_init(&(ctx->thr_wcond), NULL);
+    ctx->write_ready_head = NULL;
+    ctx->write_ready_tail = NULL;
+    ctx->read_ready_head = NULL;
+    ctx->read_ready_tail = NULL;
+#endif
+
     return 0;
 }
 
@@ -84,6 +129,15 @@ static void _vhd2raw_ctx_deinit(struct _vhd2raw_ctx *ctx)
     {
         free(ctx->compare_buf);
     }
+
+#ifdef BUILD_WITH_THREADS
+
+    /* FIXME: Cleanup read buffers */
+
+    pthread_mutex_destroy(&(ctx->thr_lock));
+    pthread_cond_destroy(&(ctx->thr_rcond));
+    pthread_cond_destroy(&(ctx->thr_wcond));
+#endif
 }
 
 static int _open_output(struct _vhd2raw_ctx *ctx)
@@ -185,7 +239,8 @@ static int _write_output(struct _vhd2raw_ctx *ctx, void *read_buf,
     return 0;
 }
 
-static int _read_and_write(struct _vhd2raw_ctx *ctx, vhd_context_t *vhd)
+static int _write_without_thread(struct _vhd2raw_ctx *ctx,
+                                 vhd_context_t *vhd)
 {
     uint64_t cur_read_pos = 0;
     uint64_t read_size_left = vhd->footer.curr_size;
@@ -222,6 +277,166 @@ static int _read_and_write(struct _vhd2raw_ctx *ctx, vhd_context_t *vhd)
     return err;
 }
 
+#ifdef BUILD_WITH_THREADS
+static void _writer_thread(struct _vhd2raw_ctx *ctx)
+{
+    struct _read_buffer_list *buf_entry;
+    void *cur_buf;
+    int err = 0;
+
+    pthread_mutex_lock(&(ctx->thr_lock));
+    while(ctx->write_ready_head || !ctx->done_reading)
+    {
+        if (!ctx->write_ready_head)
+        {
+            pthread_cond_wait(&(ctx->thr_wcond), &(ctx->thr_lock));
+            continue;
+        }
+
+        buf_entry = ctx->write_ready_head;
+        ctx->write_ready_head = ctx->write_ready_head->next;
+        if (ctx->write_ready_head == NULL)
+        {
+            ctx->write_ready_tail = NULL;
+        }
+
+        pthread_mutex_unlock(&(ctx->thr_lock));
+
+        err = _write_output(ctx, buf_entry->read_buf,
+                            buf_entry->read_bytes);
+
+        buf_entry->next = NULL;
+
+        pthread_mutex_lock(&(ctx->thr_lock));
+
+        if (ctx->read_ready_tail != NULL)
+        {
+            ctx->read_ready_tail->next = buf_entry;
+        }
+        else
+        {
+            ctx->read_ready_head = buf_entry;
+        }
+
+        ctx->read_ready_tail = buf_entry;
+
+        pthread_cond_signal(&(ctx->thr_rcond));
+
+        if (err)
+        {
+            break;
+        }
+    }
+
+    ctx->write_result = err;
+
+    pthread_mutex_unlock(&(ctx->thr_lock));
+}
+
+static int _write_with_thread(struct _vhd2raw_ctx *ctx, vhd_context_t *vhd)
+{
+    uint64_t cur_read_pos = 0;
+    uint64_t read_size_left = vhd->footer.curr_size;
+    size_t read_bytes;
+    struct _read_buffer_list *buf_entry;
+    pthread_t thr_id;
+    int err;
+    int num_buffers = 0;
+
+    err = pthread_create(&thr_id, NULL, (void *(*)(void *))_writer_thread,
+                         ctx);
+    if (err)
+        return err;
+
+
+    pthread_mutex_lock(&(ctx->thr_lock));
+
+    while (read_size_left)
+    {
+        buf_entry = ctx->read_ready_head;
+        if (buf_entry == NULL)
+        {
+            if (num_buffers == ctx->opts.read_ahead_blocks)
+            {
+                pthread_cond_wait(&(ctx->thr_rcond), &(ctx->thr_lock));
+                continue;
+            }
+
+            num_buffers++;
+
+            pthread_mutex_unlock(&(ctx->thr_lock));
+
+            buf_entry = malloc(sizeof(struct _read_buffer_list));
+            if (buf_entry == NULL)
+            {
+                err = errno ? -errno : -ENOMEM;
+                break;
+            }
+
+            err = posix_memalign(&(buf_entry->read_buf), VHD_SECTOR_SIZE,
+                                 VHD_BLOCK_SIZE);
+            if (err)
+            {
+                err = -err;
+                free(buf_entry);
+                break;
+            }
+        }
+        else
+        {
+            ctx->read_ready_head = buf_entry->next;
+            if (!ctx->read_ready_head)
+            {
+                ctx->read_ready_tail = NULL;
+            }
+
+            pthread_mutex_unlock(&(ctx->thr_lock));
+        }
+
+        buf_entry->read_bytes = (read_size_left > VHD_BLOCK_SIZE) ?
+                                 VHD_BLOCK_SIZE : read_size_left;
+        err = vhd_io_read_bytes(vhd, buf_entry->read_buf,
+                                buf_entry->read_bytes, cur_read_pos);
+        if (err < 0)
+        {
+            free(buf_entry->read_buf);
+            free(buf_entry);
+            break;
+        }
+
+        cur_read_pos += read_bytes;
+        read_size_left -= read_bytes;
+
+        buf_entry->next = NULL;
+
+        pthread_mutex_lock(&(ctx->thr_lock));
+
+        if (ctx->write_ready_tail != NULL)
+        {
+            ctx->write_ready_tail->next = buf_entry;
+        }
+        else
+        {
+            ctx->write_ready_head = buf_entry;
+        }
+
+        ctx->write_ready_tail = buf_entry;
+
+        pthread_cond_signal(&(ctx->thr_wcond));
+    }
+
+    ctx->done_reading = 1;
+    pthread_cond_signal(&(ctx->thr_wcond));
+
+    pthread_mutex_lock(&(ctx->thr_lock));
+
+    pthread_join(thr_id, NULL);
+
+    return err ? err : ctx->write_result;
+}
+    
+#endif
+
 static int _vhd_to_raw(struct _vhd2raw_ctx *ctx)
 {
     int err;
@@ -253,7 +468,16 @@ static int _vhd_to_raw(struct _vhd2raw_ctx *ctx)
     printf("Converting image of size %llu...\n",
            (unsigned long long)vhd.footer.curr_size);
 
-    err = _read_and_write(ctx, &vhd);
+#ifdef BUILD_WITH_THREADS
+    if (ctx->opts.threads)
+    {
+        err = _write_with_thread(ctx, &vhd);
+    }
+    else
+#endif
+    {
+        err = _write_without_thread(ctx, &vhd);
+    }
 
     vhd_close(&vhd);
 
@@ -274,7 +498,7 @@ vhd_util_vhd2raw(int argc, char **argv)
     _vhd2raw_ctx_init(&vhd2raw_ctx);
 
 	optind = 0;
-	while ((c = getopt(argc, argv, "b:dfhSP")) != -1) {
+	while ((c = getopt(argc, argv, "b:r:dfthSP")) != -1) {
 		switch (c) {
         case 'b':
             vhd2raw_ctx.opts.blk_size = atoi(optarg);
@@ -298,6 +522,28 @@ vhd_util_vhd2raw(int argc, char **argv)
         case 'f':
             vhd2raw_ctx.opts.force = 1;
             break;
+        case 'r':
+#ifdef BUILD_WITH_THREADS
+            vhd2raw_ctx.opts.read_ahead_blocks = atoi(optarg);
+            if ((vhd2raw_ctx.opts.read_ahead_blocks <= 0) ||
+                (vhd2raw_ctx.opts.read_ahead_blocks > 100))
+            {
+                printf("Error: read_ahead_blocks must be > 0 and <= 100\n");
+                return -EINVAL;
+            }
+            break;
+#else
+            printf("Error: threads are not available.\n");
+            return -EINVAL;
+#endif
+        case 't':
+#ifdef BUILD_WITH_THREADS
+            vhd2raw_ctx.opts.threads = 1;
+            break;
+#else
+            printf("Error: threads are not available.\n");
+            return -EINVAL;
+#endif
         case 'S':
             vhd2raw_ctx.opts.sparse = 0;
             break;
